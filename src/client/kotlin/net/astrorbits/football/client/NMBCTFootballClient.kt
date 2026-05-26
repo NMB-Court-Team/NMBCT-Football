7cbdb77354cc84417d4eb149de96f27be7ab4ae4
Miccebe <miccebe@qq.com>
2026-05-26 23:13:52 +0800

    initial commit

---
+++ src/client/kotlin/net/astrorbits/football/client/NMBCTFootballClient.kt
@@ -0,0 +1,9 @@
+package net.astrorbits.football.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object NMBCTFootballClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
