f0764a21027255711a5553f8868f0f18580f7068
ZTUmber <1543169063@qq.com>
2026-06-02 15:16:26 +0800

    tweak(SlideTackleSessions): 延长滑铲最短时间

--- src/main/kotlin/net/astrorbits/football/input/SlideTackleSessions.kt
+++ src/main/kotlin/net/astrorbits/football/input/SlideTackleSessions.kt
@@ -35,7 +35,7 @@ data class TackledResistance(
 
 object SlideTackleSessions {
     // how many ticks at least will stay in slide pose after pressed key
-    private const val MIN_SLIDE_TICKS = 15L
+    private const val MIN_SLIDE_TICKS = 30L
     private const val SLIDE_COOLDOWN_TICKS = 14L
     private const val TOUCH_RANGE_BONUS = 0.75
     private const val CHIP_HEIGHT_THRESHOLD = 0.55
